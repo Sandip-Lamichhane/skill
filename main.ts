18e8fd9dda39c51422aa4e03fafe6d9d9f79aa77
Sandip-Lamichhane <sandiplamichhane555@gmail.com>
2025-09-06 22:56:22 +0545

    full website completed, ready for deploy

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 async function loadHeader() {
-  const response = await fetch("utils/header.html");
+  const response = await fetch("./utils/header.html"); // add ./ for relative path
   const headerHtml = await response.text();
   document.getElementById("header")!.innerHTML = headerHtml;
 }
 
-loadHeader();
-
 async function loadFooter() {
-  const response = await fetch("utils/footer.html");
+  const response = await fetch("./utils/footer.html");
   const footerHtml = await response.text();
   document.getElementById("footer")!.innerHTML = footerHtml;
 }
 
+loadHeader();
 loadFooter();
